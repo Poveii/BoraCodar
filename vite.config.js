8ec0804036172b6a28c28fd73caa14150bcefc35
Poveii <yellowtutors@gmail.com>
2023-01-11 08:58:20 -0300

    :tada: init: First Commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+export default defineConfig({
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+      },
+    },
+  },
+  plugins: [react()],
+});
