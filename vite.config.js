df3d0b7f8b5f4cbf6610e021736bd06f580285f5
Poveii <yellowtutors@gmail.com>
2023-03-03 16:10:13 -0300

    fix: Removed vite routes on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const root = resolve(__dirname, "src");
-const outDir = resolve(__dirname, "dist");
 export default defineConfig({
-  root,
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(root, "index.html"),
-        project01: resolve(root, "project01/index.html"),
-      },
-    },
-  },
   plugins: [react()],
 });
