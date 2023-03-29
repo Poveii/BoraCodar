a2836e5a4b2ebb38693c5851743611f83e8ec7c5
Poveii <yellowtutors@gmail.com>
2023-03-29 16:25:04 -0300

    refactor: Give App div to each project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { Outlet } from "react-router-dom";
-import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Outlet />
-    </div>
+    <Outlet />
   );
 }
 
--- src/pages/project01/Project01.jsx
+++ src/pages/project01/Project01.jsx
@@ -58,45 +58,49 @@ export function Project01() {
   }
 
   return (
-    <div className={`card ${cardState}`}>
-      <div className="header">
-        <img
-          src={coverImage}
-          alt="Capa da música Acorda Devinho, é como um monte de amoebas de cor roxa com um fundo lilás"
-          onClick={() => {
-            if (window.outerWidth >= 768) {
-              cardState == "collapsed"
-                ? SetCardState("")
-                : SetCardState("collapsed");
-            }
-          }}
-        />
-        <div>
-          <h3>Acorda Devinho</h3>
-          <p>Banda Rocketseat</p>
+    <div className="App">
+      <div className={`card ${cardState}`}>
+        <div className="header">
+          <img
+            src={coverImage}
+            alt="Capa da música Acorda Devinho, é como um monte de amoebas de cor roxa com um fundo lilás"
+            onClick={() => {
+              if (window.outerWidth >= 768) {
+                cardState == "collapsed"
+                  ? SetCardState("")
+                  : SetCardState("collapsed");
+              }
+            }}
+          />
+          <div>
+            <h3>Acorda Devinho</h3>
+            <p>Banda Rocketseat</p>
+          </div>
         </div>
-      </div>
-      <div className="player">
-        <img src={IoniconsBackward} alt="Ícone de tocar a música anterior" />
-        <img
-          src={timelineObject.icon}
-          alt="Ícone de pausar e tocar a música"
-          onClick={() => handlePlayerIconChange(timelineObject.icon, "visible")}
-        />
-        <img src={IoniconsForward} alt="Ícone de tocar a próxima música" />
-      </div>
-      <div className={`timeline ${timelineObject.visibility}`}>
-        <input
-          type="range"
-          title="timeline"
-          min={0}
-          max={100}
-          value={timelineObject.changeValue}
-          onChange={handleTimelineChange}
-        />
-        <div>
-          <span>0:00</span>
-          <span>0:12</span>
+        <div className="player">
+          <img src={IoniconsBackward} alt="Ícone de tocar a música anterior" />
+          <img
+            src={timelineObject.icon}
+            alt="Ícone de pausar e tocar a música"
+            onClick={() =>
+              handlePlayerIconChange(timelineObject.icon, "visible")
+            }
+          />
+          <img src={IoniconsForward} alt="Ícone de tocar a próxima música" />
+        </div>
+        <div className={`timeline ${timelineObject.visibility}`}>
+          <input
+            type="range"
+            title="timeline"
+            min={0}
+            max={100}
+            value={timelineObject.changeValue}
+            onChange={handleTimelineChange}
+          />
+          <div>
+            <span>0:00</span>
+            <span>0:12</span>
+          </div>
         </div>
       </div>
     </div>
