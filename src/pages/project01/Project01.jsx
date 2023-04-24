b33ae9a7ce88425ba78edfbf53812ca8b5e4c79d
Poveii <yellowtutors@gmail.com>
2023-04-24 15:11:47 -0300

    feat: SCSS Modules to Project01

--- src/pages/project01/Project01.jsx
+++ src/pages/project01/Project01.jsx
@@ -6,7 +6,7 @@ import IoniconsBackward from "./assets/ionicons-backward.svg";
 import IoniconsForward from "./assets/ionicons-forward.svg";
 import coverImage from "./assets/cover.jpg";
 
-import "./Project01.scss";
+import styles from "./Project01.module.scss";
 
 function timelineReducer(timeline, action) {
   switch (action.type) {
@@ -35,9 +35,9 @@ function timelineReducer(timeline, action) {
 }
 
 export function Project01() {
-  const [cardState, SetCardState] = useState("collapsed");
+  const [cardState, SetCardState] = useState(styles.collapsed);
   const [timelineObject, dispatch] = useReducer(timelineReducer, {
-    visibility: "invisible",
+    visibility: styles.invisible,
     changeValue: 0,
     icon: IoniconsPlay,
   });
@@ -58,17 +58,17 @@ export function Project01() {
   }
 
   return (
-    <div className="App">
-      <div className={`card ${cardState}`}>
-        <div className="header">
+    <div className={styles.App}>
+      <div className={`${styles.card} ${cardState}`}>
+        <div className={styles.header}>
           <img
             src={coverImage}
             alt="Capa da música Acorda Devinho, é como um monte de amoebas de cor roxa com um fundo lilás"
             onClick={() => {
               if (window.outerWidth >= 768) {
-                cardState == "collapsed"
+                cardState == styles.collapsed
                   ? SetCardState("")
-                  : SetCardState("collapsed");
+                  : SetCardState(styles.collapsed);
               }
             }}
           />
@@ -77,18 +77,18 @@ export function Project01() {
             <p>Banda Rocketseat</p>
           </div>
         </div>
-        <div className="player">
+        <div className={styles.player}>
           <img src={IoniconsBackward} alt="Ícone de tocar a música anterior" />
           <img
             src={timelineObject.icon}
             alt="Ícone de pausar e tocar a música"
             onClick={() =>
-              handlePlayerIconChange(timelineObject.icon, "visible")
+              handlePlayerIconChange(timelineObject.icon, styles.visible)
             }
           />
           <img src={IoniconsForward} alt="Ícone de tocar a próxima música" />
         </div>
-        <div className={`timeline ${timelineObject.visibility}`}>
+        <div className={`${styles.timeline} ${timelineObject.visibility}`}>
           <input
             type="range"
             title="timeline"
