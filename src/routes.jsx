327823e335cff8e2000ff7e63664cc07c46c52de
Poveii <yellowtutors@gmail.com>
2023-05-12 16:17:56 -0300

    feat: Project02 Route

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,16 +3,19 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import { Project01 } from "./pages/project01/Project01";
+import { Project02 } from "./pages/project02/Project02";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "project01",
-        element: <Project01 />,
-      },
-    ],
   },
+  {
+    path: "/project01",
+    element: <Project01 />,
+  },
+  {
+    path: "/project02",
+    element: <Project02 />,
+  }
 ]);
