4592c74337d67523e20f000b36b196d5c9c1c7a8
Poveii <yellowtutors@gmail.com>
2023-05-30 15:02:24 -0300

    style: Remove unnecessary semicolons

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"
 
-import App from "./App";
+import App from "./App"
 
-import { Project01 } from "./pages/project01/Project01";
-import { Project02 } from "./pages/project02/Project02";
+import { Project01 } from "./pages/project01/Project01"
+import { Project02 } from "./pages/project02/Project02"
 
 export const router = createBrowserRouter([
   {
@@ -18,4 +18,4 @@ export const router = createBrowserRouter([
     path: "/project02",
     element: <Project02 />,
   }
-]);
+])
