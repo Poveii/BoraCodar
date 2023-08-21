81083c93335593672b246a984e10528f86a90f83
Poveii <yellowtutors@gmail.com>
2023-08-21 16:40:58 -0300

    feat: Title Metadata Project01

--- src/app/project01/page.tsx
+++ src/app/project01/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Desafio 01 - #BoraCodar',
+}
+
 export default function Project01() {
   return <div>Projeto 01</div>
 }
